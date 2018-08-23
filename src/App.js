7f378cecedb00b5266cc0051f4e956501581c223
Emily Taylor <emilytaylor23@outlook.com>
2018-08-23 14:44:31 +1000

    Set up Header and Page components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import Page from "./Page/index.js";
+
 class App extends React.Component {
   render() {
-    return <div>Hello World</div>;
+    return <Page />;
   }
 }
 
