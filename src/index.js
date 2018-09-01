f893b20946f71620ed116ff60f6474cb46469bb5
Emily Taylor <emilytaylor23@outlook.com>
2018-09-01 17:43:12 +1000

    add logo, brand, links, styling and css reset

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from './App';
+import App from "./App";
+import "./reset.css";
+import "./global.css";
 
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App />, mountNode);
-
