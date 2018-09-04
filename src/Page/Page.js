c1f5921c776fbb0994ab7a8ab427d57abdb7a787
Emily Taylor <emilytaylor23@outlook.com>
2018-09-04 14:49:27 +1000

    create add-section component

--- src/Page/Page.js
+++ src/Page/Page.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "../Header/index.js";
 import Footer from "../Footer/index.js";
+import Section from "../Section/index.js";
 
 class Page extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Section />
         <div style={{ height: "1000px", backgroundColor: "#3a1311" }}>
           This is a description body!
         </div>
