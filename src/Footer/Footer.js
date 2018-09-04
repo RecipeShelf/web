e50d2ca08ee83b20addf27e242c3f458643da0d3
Emily Taylor <emilytaylor23@outlook.com>
2018-09-04 14:19:06 +1000

    update based on review

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,40 +1,40 @@
 import React from "react";
 
 import "./Footer.scss";
-import netiflyDark from "./images/netiflyDark.svg";
+import netlifyDark from "./images/netiflyDark.svg";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div className="footer">
-        <div className="footer-content">
-          <div className="footer-top">
-            <span className="footer-links">
-              <a href="/terms" className="link link-footer">
-                Terms of Service
-              </a>
-              <a href="/privacy" className="link link-footer">
-                Privacy Policy
-              </a>
-              <a href="/credits" className="link link-footer">
-                Credits
-              </a>
-              <a href="/about" className="link link-footer">
-                About
-              </a>
-            </span>
-            <span className="copyright">Copyright © 2015 Recipe Shelf</span>
-          </div>
-          <hr />
-          <span className="footer-text">
-            By using this service, you accept our Terms of Service and Privacy
-            Policy.
-          </span>
-        </div>
-        <img className="netifly-logo" src={netiflyDark} alt="Netifly Logo" />
+const currentYear = (new Date()).getFullYear();
+
+const Footer = () => (
+  <div className="footer">
+    <div className="footer-content">
+      <div className="footer-top">
+        <span className="footer-links">
+          <a href="/terms" className="link link-footer">
+            Terms of Service
+          </a>
+          <a href="/privacy" className="link link-footer">
+            Privacy Policy
+          </a>
+          <a href="/credits" className="link link-footer">
+            Credits
+          </a>
+          <a href="/about" className="link link-footer">
+            About
+          </a>
+        </span>
+        <span className="copyright">
+          Copyright © {currentYear} Recipe Shelf
+        </span>
       </div>
-    );
-  }
-}
+      <hr />
+      <span className="footer-text">
+        By using this service, you accept our Terms of Service and Privacy
+        Policy.
+      </span>
+    </div>
+    <img className="netlify-logo" src={netlifyDark} alt="Netlify Logo" />
+  </div>
+);
 
 export default Footer;
