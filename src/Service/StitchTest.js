0ea00ed7c4ca25d300abff85750dff260d410dbf
Prasad Tadimalla <prasad.tadimalla@winning.com.au>
2018-09-08 23:11:29 +1000

    missing commit

--- src/Service/StitchTest.js
+++ src/Service/StitchTest.js
@@ -5,13 +5,11 @@ import RecipeService from "./recipe.service";
 const service = new RecipeService();
 
 const StitchTest = () => {
-  service.getRegions().then(r => console.log(r))
-  service.getRecipeSummaries().then(r => {    
-    console.log(r)
-    service.getRecipe(r[0]).then(
-      rp => console.log(rp)
-      )
-  })
+  service.getRegions().then(r => console.log(r));
+  service.getRecipeSummaries().then(r => {
+    console.log(r);
+    service.getRecipe(r[0]).then(rp => console.log(rp));
+  });
   return <div>{"asdfasf"}</div>;
 };
 
