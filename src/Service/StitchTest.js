d9a1debbd3524dc8204e3acee068758ffa823232
Prasad Tadimalla <prasad.tadimalla@winning.com.au>
2018-09-09 12:01:10 +1000

    double quote

--- src/Service/StitchTest.js
+++ src/Service/StitchTest.js
@@ -7,17 +7,17 @@ const recipeService = new RecipeService();
 const ingredientService = new IngredientService();
 
 const StitchTest = () => {
-  recipeService.getRegions().then(r => log('Recipe regions', r));
-  recipeService.getCuisines().then(r => log('Recipe cuisines', r));
-  recipeService.getCollections().then(r => log('Recipe collections', r));
+  recipeService.getRegions().then(r => log("Recipe regions", r));
+  recipeService.getCuisines().then(r => log("Recipe cuisines", r));
+  recipeService.getCollections().then(r => log("Recipe collections", r));
   recipeService.getRecipeSummaries().then(rs => {
-    log('Recipe ids', rs.map(r => r._id));
+    log("Recipe ids", rs.map(r => r._id));
     recipeService.getRecipe(rs[0]).then(rp => console.log(rp));
   });
 
-  ingredientService.getCategories().then(i => log('Ingredient categories', i));
+  ingredientService.getCategories().then(i => log("Ingredient categories", i));
   ingredientService.getIngredientSummaries().then(is => {
-    log('Ingredient ids', is.map(i => i._id));
+    log("Ingredient ids", is.map(i => i._id));
     ingredientService.getIngredient(is[0]).then(ig => console.log(ig));
   });
 
@@ -25,10 +25,8 @@ const StitchTest = () => {
 };
 
 const log = (prefix, a) => {
-  if (Array.isArray(a))
-    console.log(prefix + ': [' + a + ']')
-  else
-    console.log(prefix + ': ' + a)
+  if (Array.isArray(a)) console.log(prefix + ": [" + a + "]");
+  else console.log(prefix + ": " + a);
 };
 
 export default StitchTest;
