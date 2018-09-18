0f241fb39ebf16e9d327d09924098685d3002c03
Emily Taylor <emilytaylor23@outlook.com>
2018-09-18 22:48:07 +1000

    update background image, alter page width, fix header link widths

--- src/Page/Page.js
+++ src/Page/Page.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "../Header/index.js";
 import Footer from "../Footer/index.js";
 import Home from "../Home/index.js";
+import mockBackground from "./images/rs-back4.jpg";
 
 // TODO: Remove as the Home, Collections, Cuisines are built
 const Collections = () => <div>This is collections!</div>;
@@ -13,10 +14,15 @@ class Page extends React.Component {
   render() {
     return (
       <Router>
-        <div>
+        <div
+          style={{
+            background: `#370b09 url(${mockBackground})
+          no-repeat center center fixed`,
+            backgroundSize: "cover"
+          }}
+        > {/* Temporary styling before rotating backgrounds*/}
           <Header />
-          {/* Temporary div */}
-          <div style={{ backgroundColor: "#3a1311" }}>
+          <div className="background-overlay">
             <Route exact path="/" component={Home} />
             <Route path="/collections" component={Collections} />
             <Route path="/cuisines" component={Cuisines} />
